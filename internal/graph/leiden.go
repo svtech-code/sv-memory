1661efc99723e8e537c1d4145879cb42a91f0b7f
Mario Sandoval (Sv Tech) <sandoval.mario.08@gmail.com>
2026-07-29 00:32:35 -0400

    refactor(graph): simplify Leiden to Louvain and remove dead refine/merge code

--- internal/graph/leiden.go
+++ internal/graph/leiden.go
@@ -77,12 +77,9 @@ func (g *InMemoryGraph) LeidenDetectCommunities() map[string]int {
 	}
 
 	for iter := 0; iter < 15; iter++ {
+		prevCount := g.countCommunities(ls.community)
 		g.leidenLocalMoving(ls)
-
-		refined := g.leidenRefine(ls)
-		merged := g.leidenMerge(ls, refined)
-
-		if !g.leidenAssign(ls, merged) {
+		if g.countCommunities(ls.community) >= prevCount {
 			break
 		}
 	}
@@ -90,12 +87,8 @@ func (g *InMemoryGraph) LeidenDetectCommunities() map[string]int {
 	result := make(map[string]int)
 	remap := make(map[int]int)
 	nextID := 1
-	zeroCount := 0
 	for i, id := range ls.nodeIDs {
 		c := ls.community[i]
-		if c == 0 {
-			zeroCount++
-		}
 		if _, ok := remap[c]; !ok {
 			remap[c] = nextID
 			nextID++
@@ -155,140 +148,6 @@ func (g *InMemoryGraph) leidenLocalMoving(ls *leidenState) {
 			break
 		}
 	}
-}
-
-func (g *InMemoryGraph) leidenRefine(ls *leidenState) []int {
-	refined := make([]int, ls.n)
-	rTot := make([]float64, ls.n)
-	for i := range refined {
-		refined[i] = i
-		rTot[i] = g.degreeByIdx(i, ls)
-	}
-
-	for localIter := 0; localIter < 10; localIter++ {
-		moved := false
-		order := ls.rng.Perm(ls.n)
-		for _, idx := range order {
-			curComm := ls.community[idx]
-			curRef := refined[idx]
-			k_v := g.degreeByIdx(idx, ls)
-
-			neighborWeights := make(map[int]float64)
-			for _, edge := range g.EdgesBySource[ls.nodeIDs[idx]] {
-				if tIdx, ok := ls.nodeIndex[edge.TargetID]; ok {
-					if ls.community[tIdx] == curComm {
-						neighborWeights[refined[tIdx]] += 1.0
-					}
-				}
-			}
-			for _, edge := range g.EdgesByTarget[ls.nodeIDs[idx]] {
-				if sIdx, ok := ls.nodeIndex[edge.SourceID]; ok {
-					if ls.community[sIdx] == curComm {
-						neighborWeights[refined[sIdx]] += 1.0
-					}
-				}
-			}
-
-			bestRef := curRef
-			bestDelta := 0.0
-
-			removeDelta := -neighborWeights[curRef]/ls.m +
-				ls.gamma*k_v*(rTot[curRef]-k_v)/(2.0*ls.m*ls.m)
-
-			for nr, w := range neighborWeights {
-				if nr == curRef {
-					continue
-				}
-				addDelta := w/ls.m - ls.gamma*k_v*rTot[nr]/(2.0*ls.m*ls.m)
-				totalDelta := removeDelta + addDelta
-				if totalDelta > bestDelta+1e-12 {
-					bestDelta = totalDelta
-					bestRef = nr
-				}
-			}
-
-			if bestRef != curRef {
-				rTot[curRef] -= k_v
-				rTot[bestRef] += k_v
-				refined[idx] = bestRef
-				moved = true
-			}
-		}
-		if !moved {
-			break
-		}
-	}
-
-	refMap := make(map[int]int)
-	nextID := 0
-	for i := range refined {
-		r := refined[i]
-		if _, ok := refMap[r]; !ok {
-			refMap[r] = nextID
-			nextID++
-		}
-		refined[i] = refMap[r]
-	}
-
-	return refined
-}
-
-func (g *InMemoryGraph) leidenMerge(ls *leidenState, refined []int) []int {
-	nodeToRef := make(map[int]struct{})
-	for _, r := range refined {
-		nodeToRef[r] = struct{}{}
-	}
-	if len(nodeToRef) >= ls.n {
-		return nil
-	}
-
-	maxComm := 0
-	for _, c := range ls.community {
-		if c > maxComm {
-			maxComm = c
-		}
-	}
-
-	merged := make([]int, ls.n)
-	for i := range merged {
-		c := ls.community[i]
-		r := refined[i]
-		merged[i] = c*(maxComm+1) + r
-	}
-
-	mMap := make(map[int]int)
-	nextID := 0
-	for i := range merged {
-		m := merged[i]
-		if _, ok := mMap[m]; !ok {
-			mMap[m] = nextID
-			nextID++
-		}
-		merged[i] = mMap[m]
-	}
-
-	return merged
-}
-
-func (g *InMemoryGraph) leidenAssign(ls *leidenState, merged []int) bool {
-	if merged == nil {
-		return false
-	}
-
-	oldCount := g.countCommunities(ls.community)
-	newCount := g.countCommunities(merged)
-	if newCount >= oldCount {
-		return false
-	}
-
-	newTot := make([]float64, newCount)
-	for i, id := range ls.nodeIDs {
-		newTot[merged[i]] += float64(g.degree(id))
-	}
-
-	ls.community = merged
-	ls.tot = newTot
-	return true
 }
 
 func (g *InMemoryGraph) countCommunities(community []int) int {
